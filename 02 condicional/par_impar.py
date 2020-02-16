bda17de1c0d4ea2b59939528c6243b2db891de4c
Cursos de Computación <cursoscomputacion2020@gmail.com>
2020-02-16 12:24:09 -0400

    source code

---
+++ 02 condicional/par_impar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 09:05:33 2019
+
+@author: bolivar
+"""
+# Entrada de datos
+n = int(input('Ingrese un número: '))
+
+# Proceso y Salida
+if n % 2 == 0:
+    print('El número es par')
+else:
+    print('El número es impar')
+
+# Fin del programa
+input('Pulse una tecla para finalizar... ') # Pausa
